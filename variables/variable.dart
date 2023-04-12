06ac5d9fcdbc0c2ae2f914b2b3b1b83aa71f9476
yoon <leeyt1201@gmail.com>
2023-04-13 00:26:03 +0900

    기본타입 변수와 dynamic 변수

---
+++ variables/variable.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = 'yoon';
+  Object obj = 'obj';
+  String str = 'str';
+  int number = 1;
+  double pi = 3.14;
+  bool flag = false;
+
+  dynamic v;
+  if (v is String) {}
+
+  if (v is int) {}
+}
