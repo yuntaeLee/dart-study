c107ac5201cb21a5e70273be2da0bb120de43849
yoon <leeyt1201@gmail.com>
2023-04-14 16:21:01 +0900

    where

--- collections/List.dart
+++ collections/List.dart
@@ -17,7 +17,11 @@ void main() {
   // Combine Two Or More List
   List<String> names = ["Raj", "John", "Rocky"];
   List<String> names2 = ["Mike", "Subash", "Mark"];
-
   List<String> allNames = [...names, ...names2];
   print(allNames);
+
+  // Where
+  List<int> nums = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> evens = nums.where((n) => n.isEven).toList();
+  print(evens);
 }
