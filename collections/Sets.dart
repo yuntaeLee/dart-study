28d6670bcccfdda4d077219a366a432286cef75c
yoon <leeyt1201@gmail.com>
2023-04-14 16:26:37 +0900

    Difference and Intersection

--- collections/Sets.dart
+++ collections/Sets.dart
@@ -1,4 +1,17 @@
 void main() {
   Set<int> numbers = {1, 2, 3, 3, 4};
-  print(numbers);
+  print(numbers.contains(1));
+
+  for (int number in numbers) {
+    print(number);
+  }
+
+  Set<String> fruits1 = {"Apple", "Orange", "Mango"};
+  Set<String> fruits2 = {"Apple", "Grapes", "Banana"};
+
+  // Difference
+  print(fruits1.difference(fruits2));
+
+  // Intersection
+  print(fruits1.intersection(fruits2));
 }
